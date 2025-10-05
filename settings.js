d61b01437234d969b473b3e7b93bc733864cf55a
moohoria74 <moohoria74@gmail.com>
2025-10-05 23:42:01 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'yeet, // Your name
-  ownerNumber: '254107 204549', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+254107 204549', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
